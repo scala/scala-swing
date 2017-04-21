d4f46f567903d191c421c183628abcc9f8fd8b8d
Lukas Rytz <lukas.rytz@gmail.com>
2017-04-21 14:05:26 +0200

    Upgrade scala-module-plugin, use scalaVersionsByJvm
    - Update scala-module-plugin to 1.0.6
    - Use scalaVersionsByJvm to set crossScalaVersions and scalaVersion
    - scalaModuleOsgiSettings is now enabled by the plugin
    - Require the JVM version when building an old release against a new Scala
      version by adding it to the tag, e.g., v0.1.2#2.13.0-M1#8

--- admin/build.sh
+++ admin/build.sh
@@ -12,21 +12,31 @@ set -e
 # version 1.2.3 using all Scala versions in build.sbt's `crossScalaVersions`.
 
 # When a new, binary incompatible Scala version becomes available, a previously released version
-# can be released using that new Scala version by creating a new tag containing the Scala version
-# after a hash, e.g., v1.2.3#2.13.0-M1.
+# can be released using that new Scala version by creating a new tag containing the Scala and the
+# JVM version after hashes, e.g., v1.2.3#2.13.0-M1#8. The JVM version needs to be listed in
+# `.travis.yml`, otherwise the required build doesn't run.
 
 verPat="[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)?"
-tagPat="^v$verPat(#$verPat)?$"
+tagPat="^v$verPat(#$verPat#[0-9]+)?$"
 
-if [ "$TRAVIS_JDK_VERSION" == "$PUBLISH_JDK" ] && [[ "$TRAVIS_TAG" =~ $tagPat ]]; then
-  echo "Going to release from tag $TRAVIS_TAG!"
+if [[ "$TRAVIS_TAG" =~ $tagPat ]]; then
+  currentJvmVer=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | sed 's/^1\.//' | sed 's/[^0-9].*//')
 
   tagVer=$(echo $TRAVIS_TAG | sed s/#.*// | sed s/^v//)
   publishVersion='set every version := "'$tagVer'"'
 
-  scalaVer=$(echo $TRAVIS_TAG | sed s/[^#]*// | sed s/^#//)
-  if [ "$scalaVer" != "" ]; then
-    publishScalaVersion='set every crossScalaVersions := Seq("'$scalaVer'")'
+  scalaAndJvmVer=$(echo $TRAVIS_TAG | sed s/[^#]*// | sed s/^#//)
+  if [ "$scalaAndJvmVer" != "" ]; then
+    scalaVer=$(echo $scalaAndJvmVer | sed s/#.*//)
+    jvmVer=$(echo $scalaAndJvmVer | sed s/[^#]*// | sed s/^#//)
+    if [ "$jvmVer" != "$currentJvmVer" ]; then
+      echo "Not publishing $TRAVIS_TAG on Java version $currentJvmVer."
+      exit 0
+    fi
+    publishScalaVersion='set every scalaVersionsByJvm := Map('$jvmVer' -> List("'$scalaVer'" -> true))'
+    echo "Releasing $tagVer using Scala $scalaVer on Java version $jvmVer."
+  else
+    echo "Releasing $tagVer on Java version $currentJvmVer according to 'scalaVersionsByJvm' in build.sbt."
   fi
 
   extraTarget="+publish-signed"
